f2f5e965d075ee88a845668605b6bc06ee1976c8
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-04-28 20:16:53 +0200

    Precis innan jag skapat kontroller

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Product
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public int Price { get; set; }
+	public DateTime OrderDate { get; set; }
+	public string Category { get; set; }
+	public string Shelf { get; set; }
+	public string Description { get; set; }
+
+	public Product()
+	{
+	
+	}
+}
